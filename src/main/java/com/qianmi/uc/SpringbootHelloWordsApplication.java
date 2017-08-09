b4c581a9ef12160a523aee9f5511a89f33abfb93
lilonglong <lilonglong@qianmi.com>
2017-08-09 17:42:06 +0800

    初始化

---
+++ src/main/java/com/qianmi/uc/SpringbootHelloWordsApplication.java
@@ -0,0 +1,14 @@
+package com.qianmi.uc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringbootHelloWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloWordsApplication.class, args);
+	}
+}
